e51e320a754b1e8ae5277b403b543031f2ff0091
JFH09 <julanfzh@gmail.com>
2023-07-12 11:59:06 -0500

    Se refactoriza la logica de login, ahora cuenta con hasheo de contraseña utilizando bcrypt, de igual forma, se implementa passport para el login y register del aplicativo, y se añade la opcion de registro e inicio de sesión rapida con github

---
+++ src/dao/models/user.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const collection = "users";
+
+const userSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  email: String,
+  age: Number,
+  password: String,
+  rol: String,
+});
+
+const userModel = mongoose.model(collection, userSchema);
+export default userModel;
